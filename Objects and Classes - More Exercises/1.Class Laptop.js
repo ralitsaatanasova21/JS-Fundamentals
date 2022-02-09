c8b5709605e8dd013a7391db988c2cd64cf9b0bc
Ralitsa Atanasova <97174296+ralitsaatanasova21@users.noreply.github.com>
2022-02-09 12:57:14 +0200

    Add files via upload

---
+++ Objects and Classes - More Exercises/1.Class Laptop.js
@@ -0,0 +1,40 @@
+function solve() {
+  class Laptop {
+    isOn = false;
+    quality = 0;
+    info = {};
+    constructor(info, quality) {
+      (this.info = {
+        producer: info.producer,
+        age: info.age,
+        brand: info.brand,
+      }),
+        (this.quality = quality);
+    }
+    turnOn() {
+      this.quality -= 1;
+      return (this.isOn = true);
+    }
+    turnOff() {
+        this.quality -= 1;
+      return (this.isOn = false);
+    }
+    showInfo() {
+      return JSON.stringify(this.info);
+    }
+    get price() {
+      return 800 - this.info.age * 2 + this.quality * 0.5;
+    }
+  }
+
+  let info = { producer: "Dell", age: 2, brand: "XPS" };
+  let laptop = new Laptop(info, 10);
+  laptop.turnOn();
+  console.log(laptop.showInfo());
+  laptop.turnOff();
+  console.log(laptop.quality);
+  laptop.turnOn();
+  console.log(laptop.isOn);
+  console.log(laptop.price);
+}
+solve();
